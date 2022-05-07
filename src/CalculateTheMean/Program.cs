3d756a63ab637c899dec5240cb6dfb67fdd4bc31
Dragos <77962711+DragosOficial@users.noreply.github.com>
2022-05-07 08:56:11 +0200

    Add files via upload

---
+++ src/CalculateTheMean/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CalculateYourGradeAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a;
+            float sum = 0;
+            int i = 0;
+            int user = 0;
+            bool valid = false;
+
+            Console.WriteLine("Aby zakończyć działania wpisz do wartości 0.");
+
+            do
+            {
+                Console.WriteLine("\nPodaj liczbę:");
+                a = float.Parse(Console.ReadLine());
+                if(a == 0)
+                {
+                    break;
+                }
+                if(a <= 0)
+                {
+                    Console.WriteLine("Podaj prawidłową liczbę!");
+                }
+                sum += a;
+                i++;
+            } while(a != 0);
+
+            Console.WriteLine("Średnia to: {0}", Math.Round(sum / i, 2));
+        }
+    }
+}
